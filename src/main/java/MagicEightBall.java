6c44b80b9991bb14eb5454a5a9457af05f717c96
Amy <amy.jeanette.morrson@gmail.com>
2018-01-08 16:50:30 +0000

    added 3 answers

--- src/main/java/MagicEightBall.java
+++ src/main/java/MagicEightBall.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class MagicEightBall {
+
+    ArrayList<String> answers;
+
+    public MagicEightBall(ArrayList<String> answers) {
+        this.answers = answers;
+    }
+
+    public int numberOfAnswers() {
+        return this.answers.size();
+    }
 }
--- src/test/java/MagicEightballTest.java
+++ src/test/java/MagicEightballTest.java
@@ -1,2 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class MagicEightballTest {
+    MagicEightBall ball;
+
+    @Before
+    public void before(){
+        ArrayList<String> answers = new ArrayList<String>();
+        answers.add("Absolutely 100% yes!");
+        answers.add("Naw");
+        answers.add("Get some perspective");
+        ball = new MagicEightBall(answers);
+    }
+
+    @Test
+    public void ballHasThreeAnswers(){
+        assertEquals(3, ball.numberOfAnswers());
+    }
+
 }
